b07aa5ecb2ddfd90771bb3c2379698cd81f6a5b5
Nivas Kumar S <163018894+nivask2003@users.noreply.github.com>
2024-10-14 15:45:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,10 @@ require('dotenv').config();
 const port = 5001;
 
 const app = express();
-app.use(cors(
-    [origin: 'https://ms-customer-management.netlify.app']
-));
+app.use(cors({
+    origin: 'https://ms-customer-management.netlify.app'
+}));
+
 app.use(express.json()); // Middleware to parse JSON bodies
 
 const client = new MongoClient(process.env.MONGODB_URI);
